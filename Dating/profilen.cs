bd32c37399bbf8df1daeedc95d1a4429932e2f57
TobiasHBrandt <tobias.h.brandt@hotmail.com>
2018-05-24 13:14:42 +0200

    profil

---
+++ Dating/profilen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dating
+{
+    class profilen
+    {
+        public int ProfilID { get; set; }
+        public string Profiltekst { get; set; }
+        public bool Profilbillede { get; set; }
+
+        public void OpdaterProfil()
+        {
+
+        }
+
+        public void UploadBillede()
+        {
+
+        }
+
+        public void SletBillede()
+        {
+
+        }
+    }
+}
